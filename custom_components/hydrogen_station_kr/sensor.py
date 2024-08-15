ae9cb5cf0bf93476a9003a77ac47338fe45a1902
HAN, Sang-uk <40167504+nara1111@users.noreply.github.com>
2024-08-15 17:00:00 +0900

    Update sensor.py
    - 매 5분 단위 10초에 업데이트 하도록 변경 (수소 API 업데이트주기가 매 5분 단위 05초라 이보다 늦은 10초에 호출하는 것으로 고정)
    - 실시간 API 중 운영상태명(oper_sttus_nm) 대신 POS상태명(pos_sttus_nm)을 사용하도록 변경

--- custom_components/hydrogen_station_kr/sensor.py
+++ custom_components/hydrogen_station_kr/sensor.py
@@ -1,17 +1,16 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 import logging
+import asyncio
+import requests
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, CoordinatorEntity
-import requests
 
 from .const import DOMAIN, CONF_STATION_NAME, CONF_API_KEY
 
 _LOGGER = logging.getLogger(__name__)
-
-SCAN_INTERVAL = timedelta(minutes=2)
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -32,45 +31,46 @@ class HydrogenStationCoordinator(DataUpdateCoordinator):
             hass,
             _LOGGER,
             name="Hydrogen Station KR",
-            update_interval=SCAN_INTERVAL,
+            update_method=self._async_update_data,
         )
         self.station_name = station_name
         self.api_key = api_key
 
     async def _async_update_data(self):
-        return await self.hass.async_add_executor_job(self.fetch_data)
+        while True:
+            now = datetime.now()
+            next_five_min = now.replace(minute=(now.minute // 5) * 5, second=0, microsecond=0) + timedelta(minutes=5)
+            next_update = next_five_min + timedelta(seconds=10)
+            
+            await asyncio.sleep((next_update - now).total_seconds())
+            
+            return await self.hass.async_add_executor_job(self.fetch_data)
 
     def fetch_data(self):
         headers = {"Authorization": self.api_key}
         
-        # 실시간 정보 조회
         response = requests.get("http://el.h2nbiz.or.kr/api/chrstnList/currentInfo", headers=headers)
         current_info = next((station for station in response.json() if station["chrstn_nm"] == self.station_name), None)
 
-        # 운영 정보 조회
         response = requests.get("http://el.h2nbiz.or.kr/api/chrstnList/operationInfo", headers=headers)
         operation_info = next((station for station in response.json() if station["chrstn_nm"] == self.station_name), None)
 
         if current_info and operation_info:
-            # 이용 가능 요일 정보 변환
             use_posbl_dotw = operation_info.get("use_posbl_dotw", "")
             day_names = ["월", "화", "수", "목", "금", "토", "일", "공휴일"]
             closed_days = [day for day, is_open in zip(day_names, use_posbl_dotw) if is_open == '0']
             closed_days_str = "휴무 없음" if not closed_days else f"{', '.join(closed_days)} 휴무"
 
-            # 실시간 상태에 따른 센서 상태 결정
-            oper_sttus_nm = current_info["oper_sttus_nm"]
-            if oper_sttus_nm == "운영중":
+            pos_sttus_nm = current_info["pos_sttus_nm"]
+            if pos_sttus_nm == "운영중":
                 state = current_info["cnf_sttus_nm"]
-            elif oper_sttus_nm == "영업마감":
-                state = "영업마감"
             else:
-                state = oper_sttus_nm
+                state = pos_sttus_nm
 
             return {
                 "state": state,
                 "attributes": {
-                    "운영상태": oper_sttus_nm,
+                    "POS상태": pos_sttus_nm,
                     "대기차량수": current_info["wait_vhcle_alge"],
                     "혼잡상태": current_info["cnf_sttus_nm"],
                     "운영상태갱신일자": current_info["last_mdfcn_dt"],
